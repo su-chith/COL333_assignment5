2624d2d66491f766d9e1450c889e3748d6f87824
su-chith <suchithcodingacc@gmail.com>
2023-11-16 22:46:03 +0530

    wrote 2nd version of evaluation function

--- src/engine.cpp
+++ src/engine.cpp
@@ -82,6 +82,98 @@ double alpha_beta(Board b, int depth, bool maximise, double alpha, double beta, 
     }
 }
 
+double evaluate_2(Board b, PlayerColor clr, U8 move) {
+    U8 *pieces = (U8*)(&(b.data));
+    double white_score = 0;
+    double black_score = 0;
+    if (b.in_check()) {
+        if (b.data.player_to_play == WHITE) white_score -= 1.0;
+        else black_score -= 1.0;
+        auto moveset = b.get_legal_moves();
+        if (moveset.size() == 0) {
+            if (b.data.player_to_play == WHITE) white_score -= 100.0;
+            else black_score -= 100.0;
+        }
+    }
+
+    U8 piece_pos = getp1(move);
+    U8 piece_type = b.data.board_0[piece_pos];
+    if (piece_type & PAWN) {
+        if (piece_type & WHITE) white_score += 1.0;
+        else black_score += 1.0;
+    } else if (piece_type & KING) {
+        if (piece_type & WHITE) white_score += 2.0;
+        else black_score += 2.0;
+    } else if (piece_type & BISHOP) {
+        if (piece_type & WHITE) white_score += 3.0;
+        else black_score += 3.0;
+    } else if (piece_type & ROOK) {
+        if (piece_type & WHITE) white_score += 3.0;
+        else black_score += 3.0;
+    } else if (piece_type & KNIGHT) {
+        if (piece_type & WHITE) white_score += 5.0;
+        else black_score += 5.0;
+    }
+
+    for (int i = 0; i < b.data.n_pieces; i++) {
+        U8 piece = pieces[i];
+        if (piece == DEAD) continue;
+        U8 piece_type = b.data.board_0[piece];
+        if (piece_type & PAWN) white_score += 3.0;
+        else if (piece_type & ROOK) white_score += 8.0;
+        else if (piece_type & KING) white_score += 0.1;
+        else if (piece_type & BISHOP) white_score += 10.0;
+        else if (piece_type & KNIGHT) white_score += 12.0;
+    }
+    int s = 10;
+    for (int i = 0; i < b.data.n_pieces; i++) {
+        U8 piece = pieces[i+s];
+        if (piece == DEAD) continue;
+        U8 piece_type = b.data.board_0[piece];
+        if (piece_type & PAWN) black_score += 3.0;
+        else if (piece_type & ROOK) black_score += 8.0;
+        else if (piece_type & KING) black_score += 0.1;
+        else if (piece_type & BISHOP) black_score += 10.0;
+        else if (piece_type & KNIGHT) black_score += 12.0;
+    }
+    // can add piece adjustment values
+    if (clr == WHITE) return (white_score - black_score);
+    else return (black_score - white_score);
+}
+
+
+// minimax with alpha beta pruning 2nd version
+
+double alpha_beta2(Board b, int depth, bool maximise, double alpha, double beta, PlayerColor clr, U8 move) {
+    auto moveset = b.get_legal_moves();
+    if ((depth == 3) or (moveset.size() == 0) or (b.in_check())) {
+        return evaluate_2(b, clr, move);
+    }
+    if (maximise) {
+        double max_eval = -10000.0;
+        for (auto m : moveset) {
+            Board c(b);
+            c.do_move_(m);
+            double eval = alpha_beta2(c, depth+1, false, alpha, beta, clr, m);
+            max_eval = std::max(max_eval, eval);
+            alpha = std::max(alpha, max_eval);
+            if (beta <= alpha) break;
+        }
+        return max_eval;
+    } else {
+        double min_eval = 10000.0;
+        for (auto m : moveset) {
+            Board c(b);
+            c.do_move_(m);
+            double eval = alpha_beta2(c, depth+1, true, alpha, beta, clr, m);
+            min_eval = std::min(min_eval, eval);
+            beta = std::min(beta, min_eval);
+            if (beta <= alpha) break;
+        }
+        return min_eval;
+    }
+}
+
 void Engine::find_best_move(const Board& b) {
 
     // pick a random move
@@ -97,6 +189,7 @@ void Engine::find_best_move(const Board& b) {
         double best_score = -10000.0;
         double alpha = -10000.0;
         double beta = 10000.0;
+        std::unordered_set<U16> best_moves;
         for (auto m : moveset) {
             Board newboard(b);
             newboard.do_move_(m);
@@ -104,6 +197,21 @@ void Engine::find_best_move(const Board& b) {
             if (score >= best_score) {
                 best_score = score;
                 this->best_move = m;
+                best_moves.insert(m);
+            }
+        }
+        if (best_moves.size() >= 5) {
+            double best_score2 = -10000.0;
+            double alpha2 = -10000.0;
+            double beta2 = 10000.0;
+            for (auto m : best_moves) {
+                Board newboard(b);
+                newboard.do_move_(m);
+                double score = alpha_beta2(newboard, 0, true, alpha2, beta2, clr, m);
+                if (score > best_score2) {
+                    best_score2 = score;
+                    this->best_move = m;
+                }
             }
         }
     }
