3404f4e9236e6d072c50b89b09134dee5bfca2a4
su-chith <suchithcodingacc@gmail.com>
2023-11-17 23:50:46 +0530

    after submission

--- engine.cpp
+++ engine.cpp
@@ -7,19 +7,18 @@
 #include "engine.hpp"
 #include "butils.hpp"
 
-
-
 double evaluate(Board b, PlayerColor clr) {
     U8 *pieces = (U8*)(&(b.data));
     double white_score = 0;
     double black_score = 0;
     if (b.in_check()) {
-        if (b.data.player_to_play == WHITE) white_score -= 2.0;
-        else black_score -= 2.0;
+        if (b.data.player_to_play == WHITE) white_score -= 4.0;
+        else black_score -= 4.0;
         auto moveset = b.get_legal_moves();
         if (moveset.size() == 0) {
             if (b.data.player_to_play == WHITE) white_score -= 100.0;
             else black_score -= 100.0;
+            
         }
     }
     // need to write loops to evaluate score based on board structure
@@ -102,15 +101,15 @@ double evaluate(Board b, PlayerColor clr) {
 // minimax with alpha beta pruning
 double alpha_beta(Board b, int depth, bool maximise, double alpha, double beta, PlayerColor clr) {
     auto moveset = b.get_legal_moves();
-    if ((depth == 3) or (moveset.size() == 0) or (b.in_check())) {
+    if ((depth == 3) or (moveset.size() == 0)) {
         return evaluate(b, clr);
     }
     if (maximise) {
         double max_eval = -10000.0;
         for (auto m : moveset) {
             Board c(b);
-                        c.do_move_(m);
-                        double eval = alpha_beta(c, depth+1, false, alpha, beta, clr);
+            c.do_move_(m);
+            double eval = alpha_beta(c, depth+1, false, alpha, beta, clr);
             max_eval = std::max(max_eval, eval);
             alpha = std::max(alpha, max_eval);
             if (beta <= alpha) break;
@@ -120,8 +119,8 @@ double alpha_beta(Board b, int depth, bool maximise, double alpha, double beta, 
         double min_eval = 10000.0;
         for (auto m : moveset) {
             Board c(b);
-                        c.do_move_(m);
-                        double eval = alpha_beta(c, depth+1, true, alpha, beta, clr);
+            c.do_move_(m);
+            double eval = alpha_beta(c, depth+1, true, alpha, beta, clr);
             min_eval = std::min(min_eval, eval);
             beta = std::min(beta, min_eval);
             if (beta <= alpha) break;
@@ -135,8 +134,8 @@ double evaluate_2(Board b, PlayerColor clr, U8 move) {
     double white_score = 0;
     double black_score = 0;
     if (b.in_check()) {
-        if (b.data.player_to_play == WHITE) white_score -= 2.0;
-        else black_score -= 2.0;
+        if (b.data.player_to_play == WHITE) white_score -= 20.0;
+        else black_score -= 20.0;
         auto moveset = b.get_legal_moves();
         if (moveset.size() == 0) {
             if (b.data.player_to_play == WHITE) white_score -= 100.0;
@@ -145,60 +144,45 @@ double evaluate_2(Board b, PlayerColor clr, U8 move) {
     }
 
     U8 piece_pos = getp1(move);
-    U8 piece_pos0 = getp0(move);
     bool threat = b.under_threat(piece_pos);
-    bool threat_0 = b.under_threat(piece_pos0);
     U8 piece_type = b.data.board_0[piece_pos];
     if (piece_type & PAWN) {
+        if (piece_type & WHITE) white_score += 1.0;
+        else black_score += 1.0;
+        if (threat) {
+            if (piece_type & WHITE) white_score -= 1.0;
+            else black_score -= 1.0;
+        }
+    } else if (piece_type & KING) {
         if (piece_type & WHITE) white_score += 2.0;
         else black_score += 2.0;
-        if (threat) {
-            if (piece_type & WHITE) white_score -= 4.0;
-            else black_score -= 4.0;
-        }
-        if (threat_0) {
-            if (piece_type & WHITE) white_score += 4.0;
-            else black_score += 4.0;
-        }
     } else if (piece_type & BISHOP) {
         if (piece_type & WHITE) white_score += 3.0;
         else black_score += 3.0;
         if (threat) {
-            if (piece_type & WHITE) white_score -= 10.0;
-            else black_score -= 10.0;
+            if (piece_type & WHITE) white_score -= 6.0;
+            else black_score -= 6.0;
         }   
-        if (threat_0) {
-            if (piece_type & WHITE) white_score += 10.0;
-            else black_score += 10.0;
-        }
     } else if (piece_type & ROOK) {
         if (piece_type & WHITE) white_score += 3.0;
         else black_score += 3.0;
         if (threat) {
-            if (piece_type & WHITE) white_score -= 15.0;
-            else black_score -= 15.0;
-        }
-        if (threat_0) {
-            if (piece_type & WHITE) white_score += 15.0;
-            else black_score += 15.0;
+            if (piece_type & WHITE) white_score -= 6.0;
+            else black_score -= 6.0;
         }
     } else if (piece_type & KNIGHT) {
         if (piece_type & WHITE) white_score += 5.0;
         else black_score += 5.0;
         if (threat) {
-            if (piece_type & WHITE) white_score -= 15.0;
-            else black_score -= 15.0;
-        }
-        if (threat_0) {
-            if (piece_type & WHITE) white_score += 15.0;
-            else black_score += 15.0;
+            if (piece_type & WHITE) white_score -= 6.0;
+            else black_score -= 6.0;
         }
     }
 
     // evaluation for mobility
-    // auto moveset = b.get_legal_moves();
-    // if (b.data.player_to_play == WHITE) white_score += moveset.size() * 0.1;
-    // else black_score += moveset.size() * 0.1;
+    auto moveset = b.get_legal_moves();
+    if (b.data.player_to_play == WHITE) white_score += moveset.size() * 0.1;
+    else black_score += moveset.size() * 0.1;
     
 
     for (int i = 0; i < b.data.n_pieces; i++) {
@@ -265,7 +249,7 @@ void Engine::find_best_move(const Board& b) {
     // pick a random move
     PlayerColor clr = b.data.player_to_play;
     bool search = true;
-    std::unordered_set<U16> best_moves;
+    
     auto moveset = b.get_legal_moves();
     if (moveset.size() == 0) {
         std::cout << "Could not get any moves from board!\n";
@@ -276,14 +260,18 @@ void Engine::find_best_move(const Board& b) {
         double best_score = -10000.0;
         double alpha = -10000.0;
         double beta = 10000.0;
+        std::unordered_set<U16> best_moves;
         time_t start = time(0);
         for (auto m : moveset) {
-            time_t end = time(0);
-            if (end - start > 1.5) break;
             Board newboard(b);
             newboard.do_move_(m);
-            double score = alpha_beta(newboard, 0, true, alpha, beta, clr);
-            if (score > best_score) {
+            if (newboard.get_legal_moves().size() == 0) {
+                this->best_move = m;
+                search = false;
+                break;
+            }
+            double score = alpha_beta(newboard, 0, false, alpha, beta, clr);
+            if (score >= best_score) {
                 best_score = score;
                 this->best_move = m;
                 best_moves.insert(m);
@@ -291,7 +279,7 @@ void Engine::find_best_move(const Board& b) {
             time_t end = time(0);
             if (end - start >= 1) break;
         }
-        if (best_moves.size() >= 2) {
+        if (best_moves.size() >= 5) {
             double best_score2 = -10000.0;
             double alpha2 = -10000.0;
             double beta2 = 10000.0;
